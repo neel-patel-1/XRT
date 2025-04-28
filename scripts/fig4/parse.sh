e4c417cd742dd077564a8ffc7fab1f4e371e80bf
Neel Maulik Patel <nmp83@en-ec-emerald-01.coecis.cornell.edu>
2025-04-28 01:47:32 +0000

    parse memfill/memcpy

--- scripts/fig4/parse.sh
+++ scripts/fig4/parse.sh
@@ -2,12 +2,49 @@
 
 PAYLOAD_SIZES=( 256 1024 4096 16384 65536 262144 1048576  )
 
+echo ""
+echo "gpcore memcpy"
+echo "L2D L2C LLC DRAM"
+
+for j in "${PAYLOAD_SIZES[@]}"
+do
+  for i in logs/placement_dsa_3_${j}_cstate_*;
+  do
+    grep Baseline $i;
+  done | awk '{printf("%s ", $5);} END{printf( "\n")}'
+done
+
+echo ""
 echo "dsa memcpy"
 echo "L2D L2C LLC DRAM"
 
 for j in "${PAYLOAD_SIZES[@]}"
 do
   for i in logs/placement_dsa_3_${j}_cstate_*;
+  do
+    grep Block $i;
+  done | awk '{printf("%s ", $5);} END{printf( "\n")}'
+done
+
+echo ""
+echo "gpcore memfill"
+echo "L2D L2C LLC DRAM"
+
+for j in "${PAYLOAD_SIZES[@]}"
+do
+  for i in logs/placement_dsa_4_${j}_cstate_*;
+  do
+    grep Baseline $i;
+  done | awk '{printf("%s ", $5);} END{printf( "\n")}'
+done
+
+echo ""
+echo "dsa memfill"
+echo "L2D L2C LLC DRAM"
+
+for j in "${PAYLOAD_SIZES[@]}"
+do
+  for i in logs/placement_dsa_4_${j}_cstate_*;
   do
     grep Block $i;
   done | awk '{printf("%s ", $5);} END{printf( "\n")}'
